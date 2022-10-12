1e679ee8473a4aeacbdc21267a213cee44440a84
NhanKhangJ <khangle51096@gmail.com>
2022-10-12 14:55:54 -0600

    added If/esle statement and excersises

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,22 @@
-console.log("hello")
+console.log("hello");
+
+let num = 0;
+if (num > 0){
+    console.log("positive")
+} else if(num< 0){
+    console.log("negative")
+} else if(num=0) {
+    console.log("this is number 0")
+} else{
+    console.log("this is not a number")
+}
+
+let userAge = 19;
+
+if (userAge >= 18){
+    console.log("access granted");
+} else if(userAge <18){
+    console.log("access denied")
+} else{
+    console.log("try again")
+}
