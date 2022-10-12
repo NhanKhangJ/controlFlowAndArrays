fd9486394fc142986513d7c06df290973c3fb71a
NhanKhangJ <khangle51096@gmail.com>
2022-10-12 15:56:47 -0600

    letter Grade exercises

--- exercises/app.js
+++ exercises/app.js
@@ -20,3 +20,36 @@ if (userAge >= 18){
 } else{
     console.log("try again")
 }
+
+let number;
+if (number === 0){
+    console.log("zero")
+}else if (number >0){
+    if (number > 100){
+        console.log("possitive and greater than 100 ")
+    }else if(number < 100){
+        console.log("possiive and smaller than 100")
+    } else{
+        console.log("possitive number")
+    } 
+} else if (number < 0){
+    console.log("nagative number");
+} else {
+    console.log("please put in a number")
+}
+
+let grade;
+
+if(grade >= 90){
+    console.log("A")
+} else if(grade >= 80){
+    console.log("B")
+} else if (grade >= 70){
+    console.log("C")
+} else if (grade >= 55){
+    console.log("D")
+} else if (grade <55) {
+    console.log("F")
+} else {
+    console.log("undefined")
+}
